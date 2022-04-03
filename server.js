50eb22cb3ab12f6e1430fe8889de7db9473e9b52
Nirajan Khatri Chhetri <niraxankc@gmail.com>
2022-04-03 22:09:55 +0545

    added controller and formatter function

--- server.js
+++ server.js
@@ -12,5 +12,35 @@ if (process.env.NODE_ENV !== "production") {
 }
 let key = process.env.key;
 
+app.get("/:sectionName", async (req, res) => {
+  try {
+    const data = await axios.get(
+      `https://content.guardianapis.com/search?api-key=${key}&q=${
+        req.params.sectionName
+      }&page=${
+        req.query.page || 1
+      }&show-fields=headline,standfirst,lang,publication,lastModified`
+    );
+
+    const results = formatter(data.data.response.results);
+    res.json({ ...data.data.response, results });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// w3c standard formatter
+function formatter(result) {
+  return result.map((doc) => ({
+    title: doc.fields.headline,
+    link: doc.webUrl,
+    description: doc.fields.standfirst,
+    language: doc.fields.lang,
+    pubDate: doc.webPublicationDate,
+    lastBuildDate: doc.fields.lastModified,
+    item: [],
+  }));
+}
+
 const port = process.env.port || 8000;
 app.listen(port, () => console.log("Server running successfully"));
